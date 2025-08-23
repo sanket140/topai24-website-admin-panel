8576321e9c336e83d464aa0a95d098ff3e88c572
sanket140 <intern3.topai24@gmail.com>
2025-08-23 16:06:43 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/toasters";
 import { TooltipProvider } from "@/components/ui/toolstip";
 import { AuthProvider, useAuth } from '@/hooks/useAuth';
 import AdminPage from "@/pages/admin";
