986abc79e3e65ebdcacfd03e3083dd0655c0c857
Sanket ambaliya <intern3.topai24@gmail.com>
2025-08-23 17:14:57 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "./components/ui/toaster"; 
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { AuthProvider, useAuth } from "@/hooks/useAuth.tsx";
 import AdminPage from "@/pages/admin";
