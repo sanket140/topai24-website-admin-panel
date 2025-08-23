fa8fc6fb1bd38eac5d69bc0c03f992299898688e
sanket140 <intern3.topai24@gmail.com>
2025-08-23 16:44:24 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toasters";
-import { TooltipProvider } from "@/components/ui/toolstip";
 import { AuthProvider, useAuth } from '@/hooks/useAuth';
 import AdminPage from "@/pages/admin";
 import LoginForm from '@/components/auth/LoginForm';
@@ -39,9 +37,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <TooltipProvider>
           <AuthenticatedRouter />
-        </TooltipProvider>
       </AuthProvider>
     </QueryClientProvider>
   );
